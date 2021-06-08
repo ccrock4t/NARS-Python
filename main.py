cc6acea6eeadfe7c9fc052b3ddd37bde8e8c38d9
ccrock4t <15344554+ccrock4t@users.noreply.github.com>
2021-06-08 11:28:02 -0400

    make multiprocessing work with PyInstaller

--- main.py
+++ main.py
@@ -4,14 +4,88 @@
     Created: April 10, 2020
     Purpose: Program entry point
 """
-import multiprocessing
+
+
+
+import os
+import sys
 import threading
-import time
+import multiprocessing
+# Module multiprocessing is organized differently in Python 3.4+
+try:
+    # Python 3.4+
+    if sys.platform.startswith('win'):
+        import multiprocessing.popen_spawn_win32 as forking
+    else:
+        import multiprocessing.popen_fork as forking
+except ImportError:
+    import multiprocessing.forking as forking
 
 import Global
 import InputChannel
 import NARSGUI
 import NARS
+
+
+if sys.platform.startswith('win'):
+    # First define a modified version of Popen.
+    class _Popen(forking.Popen):
+        def __init__(self, *args, **kw):
+            if hasattr(sys, 'frozen'):
+                # We have to set original _MEIPASS2 value from sys._MEIPASS
+                # to get --onefile mode working.
+                os.putenv('_MEIPASS2', sys._MEIPASS)
+            try:
+                super(_Popen, self).__init__(*args, **kw)
+            finally:
+                if hasattr(sys, 'frozen'):
+                    # On some platforms (e.g. AIX) 'os.unsetenv()' is not
+                    # available. In those cases we cannot delete the variable
+                    # but only set it to the empty string. The bootloader
+                    # can handle this case.
+                    if hasattr(os, 'unsetenv'):
+                        os.unsetenv('_MEIPASS2')
+                    else:
+                        os.putenv('_MEIPASS2', '')
+
+    # Second override 'Popen' class with our modified version.
+    forking.Popen = _Popen
+
+
+class GUIProcess(multiprocessing.Process):
+    def __init__(self):
+        NARS_object = Global.Global.NARS
+        experience_task_buffer_name = str(NARS_object.experience_task_buffer)
+        experience_task_buffer_capacity = NARS_object.experience_task_buffer.capacity
+        event_buffer_name = str(NARS_object.event_buffer)
+        event_buffer_capacity = NARS_object.event_buffer.capacity
+        memory_bag_name = str(NARS_object.memory.concepts_bag)
+        memory_bag_capacity = NARS_object.memory.concepts_bag.capacity
+
+        data_structure_names = (experience_task_buffer_name, event_buffer_name, memory_bag_name)
+        data_structure_capacities = (experience_task_buffer_capacity, event_buffer_capacity, memory_bag_capacity)
+
+        # multiprocess pipe to pass objects between NARS and GUI Processes
+        pipe_gui_objects, pipe_NARS_objects = multiprocessing.Pipe()  # 2-way object request pipe
+        pipe_gui_strings, pipe_NARS_strings = multiprocessing.Pipe()  # 1-way string pipe
+        Global.Global.NARS_object_pipe = pipe_NARS_objects
+        Global.Global.NARS_string_pipe = pipe_NARS_strings
+
+        multiprocessing.Process.__init__(self,target=NARSGUI.start_gui,
+                             args=(Global.Global.gui_use_internal_data,
+                                   Global.Global.gui_use_interface,
+                                   data_structure_names,
+                                   data_structure_capacities,
+                                   pipe_gui_objects,
+                                   pipe_gui_strings),
+                             name="GUI thread",
+                             daemon=True)
+        self.start()
+
+        while not pipe_NARS_objects.poll(1.0):
+            print('Waiting for GUI Process to start')
+        pipe_NARS_objects.recv()  # received GUI ready signal
+
 
 
 def main():
@@ -20,40 +94,17 @@ def main():
         Creates threads, populates globals, and runs the NARS.
 
     """
+    # On Windows calling this function is necessary.
+    # On Linux/OSX it does nothing.
+    multiprocessing.freeze_support()
+
     # First, create the NARS
     NARS_object = NARS.NARS()
     Global.Global.NARS = NARS_object
-    experience_task_buffer_name = str(NARS_object.experience_task_buffer)
-    experience_task_buffer_capacity = NARS_object.experience_task_buffer.capacity
-    event_buffer_name = str(NARS_object.event_buffer)
-    event_buffer_capacity = NARS_object.event_buffer.capacity
-    memory_bag_name = str(NARS_object.memory.concepts_bag)
-    memory_bag_capacity = NARS_object.memory.concepts_bag.capacity
-
-    data_structure_names = (experience_task_buffer_name, event_buffer_name, memory_bag_name)
-    data_structure_capacities = (experience_task_buffer_capacity, event_buffer_capacity, memory_bag_capacity)
-
-    # multiprocess pipe to pass objects between NARS and GUI Processes
-    pipe_gui_objects, pipe_NARS_objects = multiprocessing.Pipe() # 2-way object request pipe
-    pipe_gui_strings, pipe_NARS_strings = multiprocessing.Pipe() # 1-way string pipe
-    Global.Global.NARS_object_pipe = pipe_NARS_objects
-    Global.Global.NARS_string_pipe = pipe_NARS_strings
 
     # setup internal/interface GUI
     if Global.Global.gui_use_internal_data or Global.Global.gui_use_interface:
-        GUI_thread = multiprocessing.Process(target=NARSGUI.start_gui,
-                                      args=(Global.Global.gui_use_internal_data,
-                                            Global.Global.gui_use_interface,
-                                            data_structure_names,
-                                            data_structure_capacities,
-                                            pipe_gui_objects,
-                                            pipe_gui_strings),
-                                      name="GUI thread",
-                                      daemon=True)
-        GUI_thread.start()
-        while not pipe_NARS_objects.poll(1.0):
-            print('Waiting for GUI Process to start')
-        pipe_NARS_objects.recv() # received GUI ready signal
+        GUIProcess()
 
     # launch shell input thread
     shell_input_thread = threading.Thread(target=InputChannel.get_user_input,
