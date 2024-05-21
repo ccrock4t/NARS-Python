fe21a1716ab183ca35b44fa2fa829b5e4ac3087e
ccrock4t <15344554+ccrock4t@users.noreply.github.com>
2024-05-20 23:09:20 -0400

    Fix valid term chars list to include 'K'

--- NALSyntax.py
+++ NALSyntax.py
@@ -279,7 +279,7 @@ List of valid characters that can be used in a term.
 valid_term_chars = {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'J', 'L', 'M',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", "^"
 }
